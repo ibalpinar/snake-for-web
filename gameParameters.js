4cad53740280ae463de49a8cd0fafb9d66361c4f
Ilker Balpinar <ilker@balpinar.com>
2021-12-26 02:12:30 +0300

    Enlarge the playground and make some visual enhancements

--- gameParameters.js
+++ gameParameters.js
@@ -1,6 +1,6 @@
-export const GRID_SIZE = 21;
-export const SNAKE_SPEED = 10;
-export const GROWTH_PARAMETER = 2;
+export const GRID_SIZE = 51;
+export const SNAKE_SPEED = 15;
+export const GROWTH_PARAMETER = 1;
 
 const POINT = Math.floor(GRID_SIZE / 2) + 1;
 export const STARTING_POSITION = { x: POINT, y: POINT };
